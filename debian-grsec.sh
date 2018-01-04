e63eb99e079de6b4e0f2ae70cb26d54aa2cc7513
Lunar <lunar@cypherpunk.is>
2018-01-04 09:10:08 -0600

    Update debian-grsec.sh

--- debian-grsec.sh
+++ debian-grsec.sh
@@ -2,33 +2,31 @@
 GRUB="/etc/default/grub"
 VERSION="4.9.0-4"
 
-# Make sure only root can run the script
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root, su to become root" 1>&2
-  exit 1
-fi
+  while getopts ":iu" opt; do
+  case $opt in
+  i)
 
-# Checking if curl is installed
-if [ ! -x  /usr/bin/curl ]; then
-  echo -e "\033[31mcurl Command Not Found\e[0m"
-  echo -e "\033[34mInstalling curl, Please Wait...\e[0m"
-apt-get install curl
-fi
+  # Make sure only root can run the script
+  if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root, su to become root" 1>&2
+    exit 1
+  fi
 
-# Fetching the latest script version
-read -p "Do you want to grab the latest version of the script? *RECOMMENDED* (Y/N)" REPLY
-if [ "${REPLY,,}" == "y" ]; then
-  curl https://raw.githubusercontent.com/lunarthegrey/debian-grsec/master/debian-grsec.sh -o /root/debian-grsec.sh
-  echo "Please re-run the script: bash /root/debian-grsec.sh"
+  # Checking if curl is installed
+  if [ ! -x  /usr/bin/curl ]; then
+    echo -e "\033[31mcurl Command Not Found\e[0m"
+    echo -e "\033[34mInstalling curl, Please Wait...\e[0m"
+    apt-get install curl
+  fi
+
+  # Fetching the latest script version
+  read -p "Do you want to grab the latest version of the script? *RECOMMENDED* (Y/N) " REPLY
+  if [ "${REPLY,,}" == "y" ]; then
+    curl https://raw.githubusercontent.com/lunarthegrey/debian-grsec/master/debian-grsec.sh -o /root/debian-grsec.sh
+    echo "Please re-run the script: bash /root/debian-grsec.sh"
   exit
-fi
+  fi
 
-while [ ! $# -eq 0 ]
-do
-  case "$1" in
-
-  --install | -i)
-  
   # Updating / Upgrading System
   read -p "Do you wish to upgrade system packages? (Y/N)" REPLY
   if [ "${REPLY,,}" == "y" ]; then
@@ -39,11 +37,11 @@ do
   # Force stable package prefences, for some reason only works with apt-get
   read -p "Do you want to force stable packages system wide? *RECOMMENDED* (Y/N)" REPLY
   if [ "${REPLY,,}" == "y" ]; then
-    cat << EOF > /etc/apt/preferences.d/force-stable
-      Package: *
-      Pin: release a=stable
-      Pin-Priority: 1001
-    EOF
+cat << EOF > /etc/apt/preferences.d/force-stable
+  Package: *
+  Pin: release a=stable
+  Pin-Priority: 1001
+EOF
   fi
 
   # Add sid to sources.list and apt-get update
@@ -65,21 +63,15 @@ do
     update-grub
   fi
 
-  while true; do
-  echo
-  read -p "To apply the new grsec kernel you must reboot. Would you like to reboot now? [Y/n] " yn
-  case $yn in
-    [Yy]* ) sync; reboot; break;;
-    [Nn]* ) exit;;
-    * ) echo "Please answer yes or no.";;
-  esac
-  done
-      
-exit
-;;
-        
---update | -u)
+  read -p "To boot from the new kernel you'll need to reboot, do you want to? (Y/N) " REPLY
+  if [ "${REPLY,,}" == "y" ]; then
+    sync
+    reboot
+  fi
+  ;;
 
+  u)
+  
   apt-get update
   
   # Check if you're on 32bit or 64bit, install the correct kernel from sid and add it to grub
@@ -93,9 +85,10 @@ exit
     sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT="Advanced options for Debian GNU\/Linux>Debian GNU\/Linux, with Linux {$VERSION}-grsec-i386"/g' $GRUB
     update-grub
   fi
-      
-exit
-;;
-esac
-shift
+  ;;
+
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+  ;;
+ esac
 done
